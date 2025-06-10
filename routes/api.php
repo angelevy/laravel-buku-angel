1e6b1bc0b3a3449eddbaf5462b9533b8b7b66c96
Angel <angellevyne@gmail.com>
2025-06-11 05:43:57 +0700

    push

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/buku', action: [BukuController::class, 'index']); // GET data
-Route::get('/buku{id}', action: [BukuController::class, 'show']); // GET data
+Route::get('/buku/{id}', action: [BukuController::class, 'show']); // GET data
 Route::post('/buku/create', [BukuController::class, 'store']); // POST data
 Route::post('/buku/{id}', [BukuController::class, 'update']); // UPDATE data (pakai POST karena form-data)
 Route::delete('/buku/{id}', [BukuController::class, 'destroy']); // DELETE data
